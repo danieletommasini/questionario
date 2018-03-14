d2b279e5dc8a27a467c3b8235e05139860886fac
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-03-14 15:36:15 +0000

    index

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
     <body>
-
+        <form action="form.php">
+            <input type="text" name=""/>
+            <input type="submit" value="Submit"/>
+        </form>
     </body>
 </html>
