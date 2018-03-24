ecb84928de02c9664f15e7ad1adc604fbb0f8b84
tinowolf <32926994+tinowolf@users.noreply.github.com>
2018-03-24 18:53:51 +0100

    connessione al database
    da sistemare il $result che da problemi

--- elaboration.php
+++ elaboration.php
@@ -1,5 +1,28 @@
-<html>
-    <body>
-        
-    </body>
+<?php
+require 'connect.php';
+if (isset($_POST['class'])){
+    $class = $_POST['class'];
+
+    $query = "INSERT INTO $class (gino, ugo) VALUES ('$class', '$class')";
+    $result = mysqli_query($connection, $query);
+    if($result){
+        $smsg = "query eseguita correttamente";
+    }
+    else{
+        $fmsg ="Errore nell'esecuzione della query";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>elaboration.php</title>
+  </head>
+  <body>
+    <div class="container">
+
+    </div>
+  </body>
 </html>
