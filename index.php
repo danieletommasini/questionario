1de559863fddd908dc0f64db1da33deef41bf5a8
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-04-18 14:06:46 +0000

    domanda 8

--- index.php
+++ index.php
@@ -18,16 +18,104 @@
             
         </head>
         <div class="container">
-            <p>Il questionario è anonimo. Ti verrà chiesto di quale classe fai parte perché questo serve per
-            elaborare le risposte.<br><br>
-            Ti preghiamo di rispondere con sincerità a tutte le domande perché il tuo giudizio è molto
-            importante per la crescita ed il miglioramento della scuola.</p>
+            <b>Il questionario è anonimo. Ti verr&agrave chiesto di quale classe fai parte perch&eacute questo serve per
+            elaborare le risposte.<b><br>
+            <b>Ti preghiamo di rispondere con sincerit&agrave a tutte le domande perch&eacute il tuo giudizio &egrave molto
+            importante per la crescita ed il miglioramento della scuola.</b>
             <form class="text-center" action="elaboration.php" method="POST">
                 La classe che frequenti: 
                 <br>
                     <label class="radio-inline"><input type="radio" name="class" value="5D"> 5D</label>
                     <label class="radio-inline"><input type="radio" name="class" value="5B"> 5B</label>
                 <br>
+                    
+                    
+                    
+                   <br><br><b>Il questionario è anonimo. Ti verrà chiesto di quale classe fai parte perché questo serve per
+elaborare le risposte.</b><br>
+<u>Ti preghiamo di rispondere con sincerità a tutte le domande perché il tuo giudizio è molto
+importante per la crescita ed il miglioramento della scuola.</u><br>
+<p>La classe che frequenti:</p><br>
+<i>1 I locali della scuola sono accoglienti?</i><br>
+ <label class="radio-inline"><input type="radio" name="c1" value="1">sì, molto</label> <br>
+ <label class="radio-inline"><input type="radio" name="c1" value="2">abbastanza</label> <br>
+ <label class="radio-inline"><input type="radio" name="c1" value="3">poco</label> <br>
+ <label class="radio-inline"><input type="radio" name="c1" value="4">per niente</label> <br>
+<br>
+<i>2 I responsabili di sede della scuola sono disponibili ad ascoltare i problemi?</i><br>
+ <label class="radio-inline"><input type="radio" name="c2" value="1">sì, sempre</label> <br>
+ <label class="radio-inline"><input type="radio" name="c2" value="2">spesso</label> <br>
+ <label class="radio-inline"><input type="radio" name="c2" value="3">qualche volta</label> <br>
+ <label class="radio-inline"><input type="radio" name="c2" value="4">no,mai</label> <br>
+<i>3 I collaboratori scolastici (bidelli) sono disponibili?</i><br>
+ <label class="radio-inline"><input type="radio" name="c3" value="1">sì, semrpe</label> <br>
+ <label class="radio-inline"><input type="radio" name="c3" value="2">spesso</label> <br>
+ <label class="radio-inline"><input type="radio" name="c3" value="3">solo qualche volta</label> <br>
+ <label class="radio-inline"><input type="radio" name="c3" value="4">no, mai</label> <br>
+<i>4 Gli insegnanti spiegano in modo:</i><br>
+ <label class="radio-inline"><input type="radio" name="c4" value="1">chiaro e comprensibile</label> <br>
+ <label class="radio-inline"><input type="radio" name="c4" value="2">abbastanza chiaro e comprensibile</label> <br>
+ <label class="radio-inline"><input type="radio" name="c4" value="3">poco chiaro e comprensibile</label> <br>
+ <label class="radio-inline"><input type="radio" name="c4" value="4">per niente chiaro e comprensibile</label> <br>
+<br>
+<i>5 Il tuo rapporto con i compagni è:</i>
+<br>
+ <label class="radio-inline"><input type="radio" name="c5" value="1">amichevole e collaborativo</label> <br>
+ <label class="radio-inline"><input type="radio" name="c5" value="2">amichevole e collaborativo solo con alcuni</label> <br>
+ <label class="radio-inline"><input type="radio" name="c5" value="3">conflittuale con alcuni</label> <br>
+ <label class="radio-inline"><input type="radio" name="c5" value="4">conflittuale con quasi tutti</label> <br>
+ <label class="radio-inline"><input type="radio" name="c5" value="5">non riesco ad avere rapporti con i miei compagni</label> <br>
+<br>
+<i>6 Trovi bello essere uno studente del Fermi?</i>
+<br>
+ <label class="radio-inline"><input type="radio" name="c6" value="1">molto</label> <br>
+ <label class="radio-inline"><input type="radio" name="c6" value="2">abbastanza</label> <br>
+ <label class="radio-inline"><input type="radio" name="c6" value="3">poco</label> <br>
+ <label class="radio-inline"><input type="radio" name="c6" value="4">per niente</label> <br>
+<br>
+<i>7 I locali della scuola sono puliti?</i>
+- sì, molto
+- generalmente sì
+- poco
+- per niente
+ <label class="radio-inline"><input type="radio" name="c7" value="1">si, molto</label> <br>
+ <label class="radio-inline"><input type="radio" name="c7" value="2">generalmente si</label> <br>
+ <label class="radio-inline"><input type="radio" name="c7" value="3">poco</label> <br>
+ <label class="radio-inline"><input type="radio" name="c7" value="4">per niente</label> <br>
+<br>
+<i>8 La scuola si impegna per migliorare?</i><br>
+- molto
+- spesso
+- poco
+- per nulla
+9 I tuoi insegnanti sono disponibili a fornirti ulteriori spiegazioni e chiarimenti se non hai capito
+qualcosa?
+- sempre
+- spesso
+- raramente
+- mai
+10 Se hai bisogno di qualcosa i tuoi compagni ti aiutano?
+- moltissimo
+- abbastanza
+- poco
+- per niente
+11 Se non capisci qualcosa chiedi ulteriori spiegazioni agli insegnanti?
+- sì, a tutti
+- solo con alcuni
+- raramente
+- no, mai
+12 Ti sembra che gli spazi della scuola siano sfruttati in maniera adeguata?
+- sì, molto
+- abbastanza, si potrebbe apportare qualche miglioramento
+- poco, si potrebbero apportare diversi miglioramenti
+- per niente
+13 La scuola si impegna per l'innovazione?
+- molto
+- spesso
+- poco
+- per nulla
+14 Il tuo rapporto con gli insegnanti è:
+- collaborativo
                     <input type="submit" name="sendclass" value="INVIA"/>
             </form>
         </div>
