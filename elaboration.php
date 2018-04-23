d3abf9d869423e9062b85ea3dc2bf165848ac258
Daniele Tommasini <daniele.tommasini.dt@gmail.com>
2018-04-23 10:50:48 +0000

    admini

--- elaboration.php
+++ elaboration.php
@@ -15,7 +15,7 @@
       
         $servername = "localhost";
         $username = "root";
-        $password = "";
+        $password = "admini";
         $dbname = "questionario";
         
         try {
